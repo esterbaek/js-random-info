ef8f4d120b7436a186d6451ead992beb6f10845e
esterbaek <baekeg@mail.uc.edu>
2025-03-27 21:03:52 -0400

    Random text button added

--- random-info.js
+++ random-info.js
@@ -1,9 +1,14 @@
 // List of possible choices
-
-
+function showInfo() { 
+   var infoList = [
+      "University of Cincinnati",
+      "Lindner College of Business",
+      "Entrepreneurial Studies",
+      "<h2>Ester Baek</h2>"
+   ];
    // Generate a random number and pick that item from the array
-
+   var randomizer = Math.floor(Math.random() * infoList.length);
    
    // Write that information in the HTML element that has the ID of "info"
-
-   
+   document.getElementById("info").innerHTML = infoList[randomizer];
+};
